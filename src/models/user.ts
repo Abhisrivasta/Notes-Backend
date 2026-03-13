8e8b0c1a4ede2b1d94d24c39f178d34ed8a814c2
Abhishek Srivastava <as6338830@gmail.com>
2026-03-13 20:33:38 +0530

    Initial setup of express with typescript and folders

---
+++ src/models/user.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+{
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+
+  role: {
+    type: String,
+    enum: ["user", "guest"],
+    default: "user"
+  },
+
+
+},
+{ timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
